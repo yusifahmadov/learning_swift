fbc4ef5a097578eae394a5c2a0282537303853b0
Yusif Ahmadov <yusifakhmad@gmail.com>
2023-08-18 22:28:34 +0400

    lesson3

---
+++ MyFirstSwiftUI/MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Yusif Ahmadov on 17.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
