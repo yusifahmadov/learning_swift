6d7db1e4a733da7b5026077d5ebe251a52c10f57
Yusif Ahmadov <yusifakhmad@gmail.com>
2023-08-21 11:47:16 +0400

    Lesson 3

--- WorldRouter/WorldRouter/ContentView.swift
+++ WorldRouter/WorldRouter/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fahrenheitValue : String = ""
+    
+    let numberFormatter : NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter
+        
+    }()
+    
+    func convertToCelcius() -> String{
+        
+        if let value = Double(fahrenheitValue){
+            let fahrenheit = Measurement<UnitTemperature>(value:value,unit:.fahrenheit)
+            
+           let celciusValue = fahrenheit.converted(to: .celsius)
+            
+            return  numberFormatter.string(from: NSNumber(value: celciusValue.value)) ?? "???"
+             
+        }else{
+            return "???"
+        }
+        
+        
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("value",text: $fahrenheitValue).font(Font.system(size: 64)).multilineTextAlignment(.center).keyboardType(.decimalPad)
+            
+            Text("fahrenheit")
+            Text("is actually").foregroundColor(.teal)
+            Text(convertToCelcius()).font(Font.system(size: 64))
+            Text("degrees Celcius")
+            Spacer()
         }
         .padding()
+        .font(.title)
+        .foregroundColor(.blue)
     }
 }
 
