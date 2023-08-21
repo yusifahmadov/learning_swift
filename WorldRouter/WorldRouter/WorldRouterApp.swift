28434c8aca0bbeb24c9c5a990c21ab5f56f201d0
Yusif Ahmadov <yusifakhmad@gmail.com>
2023-08-21 11:47:13 +0400

    Lesson 3

---
+++ WorldRouter/WorldRouter/WorldRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldRouterApp.swift
+//  WorldRouter
+//
+//  Created by Yusif Ahmadov on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WorldRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
