f37f416260edc280b84752661292b48f7ccfa742
Yusif Ahmadov <yusifakhmad@gmail.com>
2023-08-24 11:06:19 +0400

    lesson 6

---
+++ WorldRouter/WorldRouter/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  WorldRouter
+//
+//  Created by Yusif Ahmadov on 24.08.23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
